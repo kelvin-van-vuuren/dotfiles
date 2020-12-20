daae8a3d30dca762307a7b393fb3fbd861cae82d
Kelvin van Vuuren <kelvin.van-vuuren@crick.ac.uk>
2020-12-20 14:24:01 +0000

    changed git setup to setup ssh keys using ssh agent and more secure ef25519 algorithm instead of rsa

--- setup_git.sh
+++ setup_git.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
 
-read -p "Setup new ssh keys? (y/n): "
-if [ ${REPLY} == "y" ] then
-	ssh-keygen -t rsa
-
-	echo "Please add this public key to Github via following link:\n"
-	echo "https://github.com/account/ssh \n"
-	read -p "Press [Enter] key after this..."
-fi
-
 read -p "Enter full name (to show on commits): " name
 read -p "Enter Github email: " email
 git config --global user.name "$name"
 git config --global user.email "$email"
+
+read -p "Setup new ssh keys and ssh-agent? (y/n): "
+if [ ${REPLY} == "y" ] 
+then
+	eval "$(ssh-agent -s)"
+	echo "Creating ssh keys. Use default name and location for key files. Make sure to add a passphrase:"
+	ssh-keygen -t ed25519 -C "$email"
+
+	rm ~/.ssh/config
+	echo "Host *\n  AddKeysToAgent yes\n  UseKeychain yes\n  IdentityFile ~/.ssh/id_ed25519" >> ~/.ssh/config
+	ssh-add -K ~/.ssh/id_ed25519
+
+	pbcopy < ~/.ssh/id_ed25519.pub
+
+	echo "Please add this public key to Github via following link:\n"
+	echo "https://github.com/account/ssh \n"
+	echo "On the top right click new SSH Key.\nEnter a name for the key in the Title field (eg: crick-macbook)\nRight click + paste in the Key field to enter your new public key (it has already been copied to your clipboard)\nFinally, press the Add SSH Key button to finish. \n"
+	read -p "Press [Enter] key to continue after doing this..."
+fi
