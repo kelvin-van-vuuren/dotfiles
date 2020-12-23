676dc1b3c944e0abc69707d4ca34347676b11fda
Kelvin van Vuuren <kelvin.van-vuuren@crick.ac.uk>
2020-12-23 00:11:19 +0000

    updating hyper setup to use apple script and check if first time setup

--- setup_hyper_settings.sh
+++ setup_hyper_settings.sh
@@ -1,3 +1,11 @@
+
+function reload_hyper {
+	osascript -e 'activate app "Hyper"' &
+	wait %1
+	sleep 2
+	osascript -e 'quit app "Hyper"'
+}
+
 rm ~/.hyper.js
 
 reload_hyper
@@ -13,18 +21,41 @@ reload_hyper
 
 reload_hyper
 
+read -p "First time setup? (y/n): " firstTimeSetup
+if [ $firstTimeSetup == "y" ] 
+then
+	read -p "Use kelvin settings as starting point? (y/n): "
+	if [ ${REPLY} == "y" ] 
+	then
+		git clone git@gist.github.com:904f8f7c3780580178027b1121545c3f.git
+		cp ./.hyper.js ~/.hyper.js
+	fi
+
+	# download from kelvin settings
+	echo "Create a secret Gist on Github via https://gist.github.com/"
+	read -p "Press [Enter] key to continue after doing this..."
+	#use kelvin settings as starting point? 
+fi
+
+echo "Create a new personal access token for this machine via https://github.com/settings/tokens/new?scopes=gist"
+read -p "Press [Enter] key to continue after doing this..."
+
 # setup access token and gist id to fetch settings
 read -p "Enter personal access token: " accessToken
-read -p "Enter gist id: " gistId
+read -p "Enter Gist ID (end section of Gist URL): " gistId
 rm ~/.hyper_plugins/.hyper-sync-settings.json
 echo "{\n\t\"personalAccessToken\": \"$accessToken\",\n\"gistId\": \"$gistId\"\n}" >> ~/.hyper_plugins/.hyper-sync-settings.json
-echo "To pull settings open Hyper and go to Plugins -> Sync Settings -> Restore Settings"
 
-reload_hyper
+osascript -e 'activate app "Hyper"' &
+wait %1
 
-function reload_hyper {
-	osascript -e 'activate app "Hyper"' &
-	wait %1
-	sleep 2
-	osascript -e 'quit app "Hyper"'
-}
+if [ $firstTimeSetup == "y" ] 
+then
+	#upload settings
+	osascript -e 'tell application "System Events" to tell process "Hyper" to click menu item "Backup Settings" of menu 1 of menu item "Sync Settings" of menu 1 of menu bar item "Plugins" of menu bar 1' 
+	delay 2
+fi
+#pull settings
+osascript -e 'tell application "System Events" to tell process "Hyper" to click menu item "Restore Settings" of menu 1 of menu item "Sync Settings" of menu 1 of menu bar item "Plugins" of menu bar 1' 
+delay 2
+osascript -e 'quit app "Hyper"'
