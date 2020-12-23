0494bf91d9a97682636b5fc4512e59f2ab1251d0
Kelvin van Vuuren <kelvin.van-vuuren@crick.ac.uk>
2020-12-23 00:55:03 +0000

    open github link to add ssh keys automatically and output copy command for public key just in case

--- setup_git.sh
+++ setup_git.sh
@@ -18,8 +18,10 @@ then
 
 	pbcopy < ~/.ssh/id_ed25519.pub
 
+	open https://github.com/account/ssh
 	echo "Please add this public key to Github via following link:\n"
 	echo "https://github.com/account/ssh \n"
 	echo "On the top right click new SSH Key.\nEnter a name for the key in the Title field (eg: crick-macbook)\nRight click + paste in the Key field to enter your new public key (it has already been copied to your clipboard)\nFinally, press the Add SSH Key button to finish. \n"
+	echo "If you need to copy the key again then open a new terminal and run: pbcopy < ~/.ssh/id_ed25519.pub"
 	read -p "Press [Enter] key to continue after doing this..."
 fi
