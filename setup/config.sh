e254e17a951eb70fea011147281c929017787c6b
Kelvin van Vuuren <kelvin.van-vuuren@crick.ac.uk>
2021-01-05 11:12:05 +0000

    added config file, set mac settings, run git setup if no ssh keys present

---
+++ setup/config.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+if [ $machine = Mac ]; then
+	sshkeys="~/.ssh/id_ed25519"
+else
+	#TODO: add linux path
+fi
--- setup/setup.sh
+++ setup/setup.sh
@@ -1,25 +1,34 @@
 #!/usr/bin/env bash
 
-echo "Mac Xcode Command Line Tools"
-xcode-select --install
+source config.sh
+
+# if [ $machine = Mac ]; then
+# 	echo "Installing Mac Xcode Command Line Tools..."
+# 	xcode-select --install
+# fi
 
 #setup homebrew and install packages / apps
 sh setup_homebrew.sh
 
+if [ $machine = Mac ]; then
+	sh mac/mac-settings.sh
+fi
+
 #setup zsh via oh my zsh and change to default shell
 sh setup_zsh.sh
 
-read -p "Setup git config? (y/n): "
-if [ ${REPLY} == "y" ] 
-then
+#if mac set system settings
+
+
+if [ ! -f $sshkeys ]; then
 	sh setup_git.sh
 fi
 
-read -p "Use hyper settings sync? (y/n): "
-if [ ${REPLY} == "y" ] 
-then
-	sh setup_hyper_settings.sh
-fi
+# read -p "Use hyper settings sync? (y/n): "
+# if [ ${REPLY} == "y" ] 
+# then
+# 	sh setup_hyper_settings.sh
+# fi
 
 read -p "Use vs-code settings sync? (y/n): "
 if [ ${REPLY} == "y" ] 
