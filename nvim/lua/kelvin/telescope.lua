cbbee5725dbcafadf174262ed5fedfe2217acd79
Kelvin van Vuuren <kelvin.van-vuuren@crick.ac.uk>
2021-05-09 10:29:05 +0100

    updated lua nvim folder and removed old setup scripts

--- nvim/lua/kelvin/telescope.lua
+++ nvim/lua/kelvin/telescope.lua
@@ -24,6 +24,7 @@ require('telescope').setup {
     }
 }
 
+--require("telescope").load_extension("git_worktree")
 require('telescope').load_extension('fzy_native')
 
 local M = {}
@@ -34,29 +35,14 @@ M.search_dotfiles = function()
     })
 end
 
-            -- map(mode, key, lua function to call)
-            --
-            -- good place to look: telescope.actions (init.lua)
-            -- lua function to call:  (gets bufnr, not necessarily needed)
-            --   require('telescope.actions.state').get_selected_entry(bufnr)
-            --   Actions just take the bufnr and give out information
-            --   require('telescope.actions').close(bufnr)
-            --
-            --   check out telescope.actions for _all the available_ supported
-            --   actions.
-            --
-            --   :h telescope.setup ->
-            --   :h telescope.builtin ->
-            --   :h telescope.layout ->
-            --   :h telescope.actions
-M.git_branches = function()
-    require("telescope.builtin").git_branches({
-        attach_mappings = function(_, map)
-            map('i', '<c-d>', actions.git_delete_branch)
-            map('n', '<c-d>', actions.git_delete_branch)
-            return true
-        end
-    })
-end
+--M.git_branches = function()
+    --require("telescope.builtin").git_branches({
+        --attach_mappings = function(_, map)
+            --map('i', '<c-d>', actions.git_delete_branch)
+            --map('n', '<c-d>', actions.git_delete_branch)
+            --return true
+        --end
+    --})
+--end
 
 return M
--- setup/install_packages.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source ../packages.sh
-$install ${packages[@]}
-
-if is_mac; then
-	ln -s "$(brew --prefix llvm)/bin/clang-tidy" "/usr/local/bin/clang-tidy"
-	brew cleanup
-fi
