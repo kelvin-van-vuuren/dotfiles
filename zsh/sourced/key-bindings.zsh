1d7d28992163d6ce9bd39f76fccd5bd81761ca2b
Kelvin van Vuuren <kelvin.van-vuuren@crick.ac.uk>
2021-05-26 12:23:09 +0100

    debloated zsh, no ohmyzsh

---
+++ zsh/sourced/key-bindings.zsh
@@ -0,0 +1,6 @@
+bindkey '^p' history-substring-search-up
+bindkey '^n' history-substring-search-down
+bindkey '^k' up-history
+bindkey '^j' down-history
+bindkey '^l' forward-char
+bindkey '^h' backward-char
