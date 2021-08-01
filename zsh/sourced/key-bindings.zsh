c7ffc0fb31e275f52758a278dcad91fe7886355e
Kelvin van Vuuren <kelvin.van-vuuren@crick.ac.uk>
2021-08-01 22:28:44 +0100

    overhaul

--- zsh/sourced/key-bindings.zsh
+++
@@ -1,6 +0,0 @@
-bindkey '^p' history-substring-search-up
-bindkey '^n' history-substring-search-down
-bindkey '^k' up-history
-bindkey '^j' down-history
-bindkey '^l' forward-char
-bindkey '^h' backward-char
